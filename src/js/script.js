94a82c9a89c429dca1676b3c1d276caa0ed65f3f
Thayna <thayna.pereira2402@gmail.com>
2025-05-26 21:48:57 -0300

    feat: create banner with slideshow

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+let imagens = [
+  '/src/assets/banner.png',
+  '/src/assets/area-alagada.png',
+  '/src/assets/voluntarios.png'
+];
+
+let i = 0;
+let tempo = 5000;
+
+function slideShow() {
+  const banner = document.getElementById('banner-container');
+  banner.style.backgroundImage = `url('${imagens[i]}')`;
+  i++;
+  if (i === imagens.length) {
+    i = 0;
+  }
+  setTimeout(slideShow, tempo);
+}
+
+slideShow();
