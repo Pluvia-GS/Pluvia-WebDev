60191ec8c4e3669c72ec315f3945666c505ed6eb
Thayna <thayna.pereira2402@gmail.com>
2025-05-30 10:39:05 -0300

    feat: add quiz page and responsive hamburger menu implementation

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,13 @@ function slideShow() {
 
 slideShow();
 
+const toggleBtn = document.getElementById('menu-toggle');
+const menu = document.querySelector('#menu nav ul');
+
+toggleBtn.addEventListener('click', () => {
+  if (menu.style.display === 'flex') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'flex';
+  }
+});
