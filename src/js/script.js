becc54868b076d2ce9323a16c411f62f8f4d87e5
Thayna <thayna.pereira2402@gmail.com>
2025-05-30 10:51:56 -0300

    feat: add background color switcher icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 let imagens = [
-  '/src/assets/banner.png',
-  '/src/assets/area-alagada.png',
-  '/src/assets/voluntarios.png'
+  "/src/assets/banner.png",
+  "/src/assets/area-alagada.png",
+  "/src/assets/voluntarios.png",
 ];
 
 let i = 0;
 let tempo = 5000;
 
 function slideShow() {
-  const banner = document.getElementById('banner-container');
+  const banner = document.getElementById("banner-container");
   banner.style.backgroundImage = `url('${imagens[i]}')`;
   i++;
   if (i === imagens.length) {
@@ -19,13 +19,22 @@ function slideShow() {
 
 slideShow();
 
-const toggleBtn = document.getElementById('menu-toggle');
-const menu = document.querySelector('#menu nav ul');
+const toggleBtn = document.getElementById("menu-toggle");
+const menu = document.querySelector("#menu nav ul");
 
-toggleBtn.addEventListener('click', () => {
-  if (menu.style.display === 'flex') {
-    menu.style.display = 'none';
+toggleBtn.addEventListener("click", () => {
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
   } else {
-    menu.style.display = 'flex';
+    menu.style.display = "flex";
   }
+});
+
+const buttons = document.querySelectorAll(".theme-btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const color = btn.getAttribute("data-color");
+    document.body.style.backgroundColor = color;
+  });
 });
